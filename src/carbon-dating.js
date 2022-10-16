731a5835c5662d3f920e36c6cd86d2662e667b2f
Andrei Kanoplich <andrei.kanoplich@gmail.com>
2022-10-16 13:30:22 +0300

    feat: add carbon dating solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,15 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
+function dateSample(sampleActivity) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+
+  if (sampleActivity < 0 || sampleActivity > 15 || typeof sampleActivity !== 'string' || !Number(sampleActivity)) {
+    return false;
+  }
+  
+  return Math.ceil(Math.log((MODERN_ACTIVITY / +sampleActivity)) / (0.693 / HALF_LIFE_PERIOD));
 }
 
 module.exports = {
